49ebd68c7588e38d759f19604bd6a5aa9e1d687a
prabh34 <77963863+prabh34@users.noreply.github.com>
2021-04-23 14:34:03 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,82 @@
+
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var helicopter;helicopterSprite;helicopterImg
+var package,packageSprite,packageImg;
+var ground;
+var packageBody;
+
+
+function preload() {
+helicopterImg = loadImage("helicopter.png");
+packageImg = loadImage("package.png");
+}
+
+
+
+
+function setup() {
+	createCanvas(800, 750);
+
+	
+
+	helicopterSprite = createSprite(400,200,13,13);
+	helicopterSprite.addImage(helicopterImg);
+	helicopterSprite.scale = 0.5;
+
+	packageSprite= createSprite(100,200,13,13);
+	packageSprite.addImage(packageImg);
+	packageSprite.scale = 0.2;
+
+	ground = createSprite(365,738,200,15);
+	ground.shapeColor=color("red");
+
+	engine = Engine.create();
+	world = engine.world;
+
+	
+	
+
+}
+
+function draw() {
+	background("black");
+
+	packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y
+
+	
+	//text(mouseX+","+mouseY,mouseX,mouseY);
+
+	drawSprites();
+
+
+
+}
+
+function keyPressed() {
+
+	if (keyCode === LEFT_ARROW) {
+		helicopter.x=helicopter.x-20;
+		translate={x:-20,y:0}
+		Matter.Body.translate(package, {x:-20,y:0})
+		
+	}
+	if (keyCode === RIGHT_ARROW) {
+		helicopter.x=helicopter.x+20;
+		translate={x:+20,y:0}
+		Matter.Body.translate(package,translate);
+	}
+
+	if (keyCode === DOWN_ARROW) {
+
+		Matter.Body.setStatic(packageBody,false); 
+	}
+
+	
+
+}
